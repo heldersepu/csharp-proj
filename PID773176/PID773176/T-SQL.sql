b6a64791cefe5fb2b78d0c8b8cc54a2501acbc39
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2011-11-16 20:39:51 +0000

    Stored procedures

---
+++ PID773176/PID773176/T-SQL.sql
@@ -0,0 +1,31 @@
+-- Read Data from the DB
+USE [QFWinData_QQ990099]
+GO
+/****** Object:  StoredProcedure [dbo].[GetAddress]    Script Date: 11/16/2011 15:35:27 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER Procedure [dbo].[GetAddress]
+as
+	SELECT top 10 [ADDRESS1] + ', ' + [CITY] + ', ' + [STATE] + ' ' + [ZIP] as [ADDRESS]
+	From CLNMAS
+	Where [STATE]= 'TX'
+
+
+-- Saves the given data to the DB
+USE [QFWinData_QQ990099]
+GO
+/****** Object:  StoredProcedure [dbo].[SetAddress]    Script Date: 11/16/2011 15:35:41 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER procedure [dbo].[SetAddress]
+	@Address varchar(250),
+	@Latitude varchar(250),
+	@Longitude varchar(250)
+as
+	UPDATE CLNMAS
+	SET Latitude = @Latitude, Longitude	= @Longitude
+	WHERE [ADDRESS1] + ', ' + [CITY] + ', ' + [STATE] + ' ' + [ZIP] = @Address
