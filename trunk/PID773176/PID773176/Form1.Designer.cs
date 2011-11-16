4d56165b3334760de78253142d5991a2093e2d12
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2011-11-16 02:46:04 +0000

    continue work on the Bing GeoData

--- trunk/PID773176/PID773176/Form1.Designer.cs
+++ trunk/PID773176/PID773176/Form1.Designer.cs
@@ -33,42 +33,46 @@
             this.txbAddressList = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fielToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadFromTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.loadFromStoredProcedureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.inputToTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.inputToSQLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFromSQLStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OutputToTextFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OutputToSQLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.eXECUTEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(381, 30);
+            this.btnAdd.Location = new System.Drawing.Point(506, 33);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(36, 22);
             this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "add";
+            this.btnAdd.Text = "&add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // txbAddress
             // 
-            this.txbAddress.Location = new System.Drawing.Point(31, 32);
+            this.txbAddress.Location = new System.Drawing.Point(39, 35);
             this.txbAddress.Name = "txbAddress";
-            this.txbAddress.Size = new System.Drawing.Size(344, 20);
+            this.txbAddress.Size = new System.Drawing.Size(461, 20);
             this.txbAddress.TabIndex = 0;
             this.txbAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbAddress_KeyDown);
             // 
             // txbAddressList
             // 
+            this.txbAddressList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.txbAddressList.BackColor = System.Drawing.SystemColors.Window;
             this.txbAddressList.Location = new System.Drawing.Point(12, 68);
             this.txbAddressList.Multiline = true;
             this.txbAddressList.Name = "txbAddressList";
             this.txbAddressList.ReadOnly = true;
             this.txbAddressList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txbAddressList.Size = new System.Drawing.Size(425, 287);
+            this.txbAddressList.Size = new System.Drawing.Size(567, 287);
             this.txbAddressList.TabIndex = 2;
             // 
             // menuStrip1
@@ -79,7 +83,7 @@
             this.eXECUTEToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(449, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(594, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -87,66 +91,77 @@
             // 
             this.fielToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.loadFromTextFileToolStripMenuItem,
-            this.loadFromStoredProcedureToolStripMenuItem});
+            this.loadFromSQLStripMenuItem});
             this.fielToolStripMenuItem.Name = "fielToolStripMenuItem";
             this.fielToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
-            this.fielToolStripMenuItem.Text = "Input";
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.inputToTextFileToolStripMenuItem,
-            this.inputToSQLToolStripMenuItem});
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.editToolStripMenuItem.Text = "Output";
-            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            this.fielToolStripMenuItem.Text = "&Input";
             // 
             // loadFromTextFileToolStripMenuItem
             // 
             this.loadFromTextFileToolStripMenuItem.Name = "loadFromTextFileToolStripMenuItem";
-            this.loadFromTextFileToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.loadFromTextFileToolStripMenuItem.Text = "Input from text file";
+            this.loadFromTextFileToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.loadFromTextFileToolStripMenuItem.Text = "Input from &Text file";
             this.loadFromTextFileToolStripMenuItem.Click += new System.EventHandler(this.loadFromTextFileToolStripMenuItem_Click);
             // 
-            // loadFromStoredProcedureToolStripMenuItem
+            // loadFromSQLStripMenuItem
             // 
-            this.loadFromStoredProcedureToolStripMenuItem.Name = "loadFromStoredProcedureToolStripMenuItem";
-            this.loadFromStoredProcedureToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
-            this.loadFromStoredProcedureToolStripMenuItem.Text = "Input from SQL";
+            this.loadFromSQLStripMenuItem.Name = "loadFromSQLStripMenuItem";
+            this.loadFromSQLStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.loadFromSQLStripMenuItem.Text = "Input from &SQL";
+            this.loadFromSQLStripMenuItem.Click += new System.EventHandler(this.loadFromSQLStripMenuItem_Click);
             // 
-            // inputToTextFileToolStripMenuItem
+            // editToolStripMenuItem
             // 
-            this.inputToTextFileToolStripMenuItem.Name = "inputToTextFileToolStripMenuItem";
-            this.inputToTextFileToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.inputToTextFileToolStripMenuItem.Text = "Output to text file";
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OutputToTextFileToolStripMenuItem,
+            this.OutputToSQLToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.editToolStripMenuItem.Text = "&Output";
             // 
-            // inputToSQLToolStripMenuItem
+            // OutputToTextFileToolStripMenuItem
             // 
-            this.inputToSQLToolStripMenuItem.Name = "inputToSQLToolStripMenuItem";
-            this.inputToSQLToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
-            this.inputToSQLToolStripMenuItem.Text = "Output to SQL";
-            this.inputToSQLToolStripMenuItem.Click += new System.EventHandler(this.inputToSQLToolStripMenuItem_Click);
+            this.OutputToTextFileToolStripMenuItem.Name = "OutputToTextFileToolStripMenuItem";
+            this.OutputToTextFileToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.OutputToTextFileToolStripMenuItem.Text = "Output to &Text file";
+            this.OutputToTextFileToolStripMenuItem.Click += new System.EventHandler(this.OutputToTextFileToolStripMenuItem_Click);
+            // 
+            // OutputToSQLToolStripMenuItem
+            // 
+            this.OutputToSQLToolStripMenuItem.Name = "OutputToSQLToolStripMenuItem";
+            this.OutputToSQLToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.OutputToSQLToolStripMenuItem.Text = "Output to &SQL";
+            this.OutputToSQLToolStripMenuItem.Click += new System.EventHandler(this.OutputToSQLToolStripMenuItem_Click);
             // 
             // eXECUTEToolStripMenuItem
             // 
             this.eXECUTEToolStripMenuItem.Name = "eXECUTEToolStripMenuItem";
             this.eXECUTEToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
-            this.eXECUTEToolStripMenuItem.Text = "EXECUTE";
+            this.eXECUTEToolStripMenuItem.Text = "E&XECUTE";
             this.eXECUTEToolStripMenuItem.Click += new System.EventHandler(this.eXECUTEToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
             // 
             // frmGeoData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 367);
+            this.ClientSize = new System.Drawing.Size(594, 371);
             this.Controls.Add(this.txbAddressList);
             this.Controls.Add(this.txbAddress);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.menuStrip1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 396);
+            this.MinimumSize = new System.Drawing.Size(600, 396);
             this.Name = "frmGeoData";
             this.Text = "GeoData";
             this.menuStrip1.ResumeLayout(false);
@@ -165,10 +180,12 @@
         private System.Windows.Forms.ToolStripMenuItem fielToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadFromTextFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem loadFromStoredProcedureToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem inputToTextFileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem inputToSQLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFromSQLStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OutputToTextFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OutputToSQLToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem eXECUTEToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
 
--- trunk/PID773176/PID773176/Form1.cs
+++ trunk/PID773176/PID773176/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,51 +21,6 @@ namespace PID773176
             InitializeComponent();
         }
 
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            if (!txbAddressList.Text.Contains(txbAddress.Text + Environment.NewLine))
-            {                
-                txbAddressList.Text = txbAddress.Text + Environment.NewLine + txbAddressList.Text;                
-            }
-            txbAddress.Text = "";
-        }
-
-        private void txbAddress_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyValue == 13)
-            {
-                btnAdd_Click(sender, null);
-            }
-        }
-
-        private void editToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void loadFromTextFileToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //Read text file
-        }
-
-        private void inputToSQLToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void eXECUTEToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            string temp = "";
-            for (int i = 0; i < txbAddressList.Lines.Count(); i++)
-            {
-                if (txbAddressList.Lines[i].Length > 5)
-                {
-                    temp = txbAddressList.Lines[i] + " = " + GeoCodeData(txbAddressList.Lines[i]) + Environment.NewLine + temp;
-                }
-            }
-            txbAddressList.Text = temp;
-        }
-        
         private String GeoCodeData(string address)
         {
             string result = "No GeoData";
@@ -90,5 +46,77 @@ namespace PID773176
             catch { }             
             return result;            
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!txbAddressList.Text.Contains(txbAddress.Text + Environment.NewLine))
+            {                
+                txbAddressList.Text = txbAddress.Text + Environment.NewLine + txbAddressList.Text;                
+            }
+            txbAddress.Text = "";
+        }
+
+        private void txbAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                btnAdd_Click(sender, null);
+            }
+        }
+
+        private void eXECUTEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string temp = "";
+            for (int i=(txbAddressList.Lines.Count()-1); i>=0; i--)
+            {
+                if (txbAddressList.Lines[i].Length > 4)
+                {
+                    temp = txbAddressList.Lines[i] + " = " + GeoCodeData(txbAddressList.Lines[i]) + Environment.NewLine + temp;
+                    this.Refresh();
+                }
+            }
+            txbAddressList.Text = temp;         
+        }
+
+        private void loadFromTextFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    txbAddressList.Text = File.ReadAllText(openFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void OutputToTextFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, txbAddressList.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not Write file to disk. Original error: " + ex.Message);
+                }
+            }            
+        }
+
+        private void loadFromSQLStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //To Do
+        }
+        
+        private void OutputToSQLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //To Do
+        }
+
     }
 }
