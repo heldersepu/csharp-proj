571ad6fcb93483f16b40579365fea39d5eda53cb
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-04-24 23:17:57 +0000

    add a new solution to read from BC api

---
+++ BCReader/BCReader/classes/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BCReader
+{
+    class Utils
+    {
+        public static void doSleep()
+        {
+            string[] chrs = { "\\", "|", "/", "*" };
+            for (int i = 0; i < 60; i++)
+            {
+                foreach (string chr in chrs)
+                {
+                    Console.Write(chr);
+                    Thread.Sleep(10);
+                    Console.Write((char)8);
+                }
+                Console.Write(".");
+                Thread.Sleep(5);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ BCReader/BCReader/classes/config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCReader
+{
+    class config
+    {
+        private XmlDocument doc;
+        public config(string strFile)
+        {
+            try
+            {
+                doc = new XmlDocument();
+                doc.Load(strFile);
+            }
+            catch
+            {
+            }        
+        }
+
+        public string store_api
+        {
+            get
+            {
+                XmlNode node = doc.SelectSingleNode("/store/api");
+                return node.InnerText;
+            }
+        }
+
+        public string store_url
+        {
+            get
+            {
+                XmlNode node = doc.SelectSingleNode("/store/url");
+                return node.InnerText;
+            }
+        }
+    }
+}
