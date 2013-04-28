400807450902e2882c53230bf793e35f594a6b58
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-04-28 13:47:04 +0000

    Add quotes to the call to be sure

--- BCReader/BCReader/doLoop.bat
+++ BCReader/BCReader/doLoop.bat
@@ -1,5 +1,7 @@
-:init
-C:\csharp-proj\BCReader\BCReader\bin\Debug\BCReader.exe C:\csharp-proj\BCReader\BCReader\sample.xml
-@echo.
-@ping 127.0.0.1 > nul
-@GOTO init
+:iSTART
+
+CALL "C:\csharp-proj\BCReader\BCReader\bin\Debug\BCReader.exe" "C:\csharp-proj\BCReader\BCReader\sample.xml"
+
+@ECHO.
+@PING 127.0.0.1 > nul
+@GOTO iSTART
