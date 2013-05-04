ad7e5efb94717839f4caae574d6baaf169e8d90f
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-05-04 21:58:37 +0000

    add a logging mechanism

---
+++ BCReader/BCReader/classes/log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace BCReader
+{
+    class log
+    {
+        private string strFile = "";
+        public log(string _strFile)
+        {
+            if (File.Exists("dolog.txt"))
+            {
+                strFile = _strFile;
+                if (strFile.EndsWith(".log", true, null))
+                {
+                    strFile += ".log";
+                }
+                if (!File.Exists(strFile))
+                {
+                    File.CreateText(strFile);
+                }
+            }
+        }
+
+        public void append(string message, string phone, long id)
+        {
+            if (strFile != "")
+            {
+                using (StreamWriter w = File.AppendText(strFile))
+                {
+                    w.WriteLine(DateTime.Now.ToString());
+                    w.WriteLine(id.ToString() + ' ' + phone + ' ' + message);
+                    w.WriteLine("   ---   ---   ---   ---");
+                }
+            }
+        }
+    }
+}
