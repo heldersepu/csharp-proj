f9a47b0e44658967e07cac531763872f305b21cd
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-05-09 03:02:40 +0000

    continue work on twitter

--- trunk/BCReader/BCReader/classes/twitter.cs
+++ trunk/BCReader/BCReader/classes/twitter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Xml;
 using System.Web;
 using System.Net;
 using System.Text;
@@ -13,6 +14,10 @@ namespace BCReader
         private string _twitterJsonUrl = "https://api.twitter.com/1.1/statuses/update.json";     
         private string _twitterUser = string.Empty;
         private string _twitterPass = string.Empty;
+        private string ConsumerKey = string.Empty;
+        private string ConsumerSecret = string.Empty;
+        private string AccessToken = string.Empty;
+        private string AccessTokenSecret = string.Empty;
         
         public twitter(string userName, string userPassword)
         {
@@ -20,72 +25,37 @@ namespace BCReader
             _twitterPass = userPassword;
         }
 
-        public string SendTwitterMessage(string message)
+        public twitter(string strDirPath)
         {
-            try
+            string strFile = strDirPath + "\\twitter.xml";
+            if (File.Exists(strFile))
             {
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(_twitterJsonUrl);
-                System.Net.ServicePointManager.Expect100Continue = false;
-             
-                string post = string.Empty;
-                using (TextWriter writer = new StringWriter())
+                try
                 {
-                    writer.Write("status={0}", HttpUtility.UrlEncode(message));
-                    post = writer.ToString();
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(strFile);
+                    ConsumerKey = doc.SelectSingleNode("/twitter/ConsumerKey").InnerText;
+                    ConsumerSecret = doc.SelectSingleNode("/twitter/ConsumerSecret").InnerText;
+                    AccessToken = doc.SelectSingleNode("/twitter/AccessToken").InnerText;
+                    AccessTokenSecret = doc.SelectSingleNode("/twitter/AccessTokenSecret").InnerText;
                 }
-             
-                SetRequestParams(request);
-                request.Credentials = new NetworkCredential(_twitterUser, _twitterPass);
-             
-                using (Stream requestStream = request.GetRequestStream())
+                catch
                 {
-                    using (StreamWriter writer = new StreamWriter(requestStream))
-                    {
-                        writer.Write(post);
-                    }
                 }
-             
-                WebResponse response = request.GetResponse();
-                string content;
-             
-                using (Stream responseStream = response.GetResponseStream())
-                {
-                    using (StreamReader reader = new StreamReader(responseStream))
-                    {
-                        content = reader.ReadToEnd();
-                    }
-                }
-                return content;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
             }
         }
         
-        public void SendTwitterMessage2(string message)
+        public void SendTwitterMessage(string message)
         {
             //GS - Get the oAuth params
             string status = HttpUtility.UrlEncode(message);
             string postBody = "status=" + Uri.EscapeDataString(status);
 
-            string oauth_consumer_key = "hj2HOF2lls1X5REebRXbjQ";
-            string consumerSecret = "BmgzVRz4AIGLF13INwPnoaUqueF32yc981GqX74Sf6U";
-
-            string oauth_token = "1412861353-6eB0SExZws32l5NSUwx1qqYEnFNO4Z185fIt7XV";
-            string oauth_token_secret = "JDRNR5Ipn2fgIbBgj5L1r7HTV70LPOCsXvVHpL1uSLM";
-
-            string oauth_nonce = Convert.ToBase64String(
-                new ASCIIEncoding().GetBytes(
-                    DateTime.Now.Ticks.ToString()));
-
+            string oauth_nonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
             string oauth_signature_method = "HMAC-SHA1";
 
-
             TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-
             string oauth_timestamp = Convert.ToInt64(ts.TotalSeconds).ToString();
-
             string oauth_version = "1.0";
 
             //GS - When building the signature string the params
@@ -96,11 +66,11 @@ namespace BCReader
 
             sd.Add("status", status);
             sd.Add("oauth_version", oauth_version);
-            sd.Add("oauth_consumer_key", oauth_consumer_key);
+            sd.Add("oauth_consumer_key", ConsumerKey);
             sd.Add("oauth_nonce", oauth_nonce);
             sd.Add("oauth_signature_method", oauth_signature_method);
             sd.Add("oauth_timestamp", oauth_timestamp);
-            sd.Add("oauth_token", oauth_token);
+            sd.Add("oauth_token", AccessToken);
 
             //GS - Build the signature string
             string baseString = String.Empty;
@@ -118,7 +88,7 @@ namespace BCReader
             baseString = baseString.Substring(0, baseString.Length - 3);
 
             //GS - Build the signing key
-            string signingKey = Uri.EscapeDataString(consumerSecret) + "&" + Uri.EscapeDataString(oauth_token_secret);
+            string signingKey = Uri.EscapeDataString(ConsumerSecret) + "&" + Uri.EscapeDataString(AccessTokenSecret);
 
             //GS - Sign the request
             HMACSHA1 hasher = new HMACSHA1(new ASCIIEncoding().GetBytes(signingKey));
@@ -134,8 +104,8 @@ namespace BCReader
             authorizationHeaderParams += "oauth_nonce=" + "\"" + Uri.EscapeDataString(oauth_nonce) + "\",";
             authorizationHeaderParams += "oauth_signature_method=" + "\"" + Uri.EscapeDataString(oauth_signature_method) + "\",";
             authorizationHeaderParams += "oauth_timestamp=" + "\"" + Uri.EscapeDataString(oauth_timestamp) + "\",";
-            authorizationHeaderParams += "oauth_consumer_key=" + "\"" + Uri.EscapeDataString(oauth_consumer_key) + "\",";
-            authorizationHeaderParams += "oauth_token=" + "\"" + Uri.EscapeDataString(oauth_token) + "\",";
+            authorizationHeaderParams += "oauth_consumer_key=" + "\"" + Uri.EscapeDataString(ConsumerKey) + "\",";
+            authorizationHeaderParams += "oauth_token=" + "\"" + Uri.EscapeDataString(AccessToken) + "\",";
             authorizationHeaderParams += "oauth_signature=" + "\"" + Uri.EscapeDataString(signatureString) + "\",";
             authorizationHeaderParams += "oauth_version=" + "\"" + Uri.EscapeDataString(oauth_version) + "\"";
             hwr.Headers.Add("Authorization", authorizationHeaderParams);
