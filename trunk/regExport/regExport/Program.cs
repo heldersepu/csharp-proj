118d67c3bd6cafc76c1f1e3d9a8c31262997da63
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-05-29 13:09:05 +0000

    Tool to export registry from C#

---
+++ trunk/regExport/regExport/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+
+namespace regExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExportReg("HKEY_LOCAL_MACHINE\\Software\\RCI", "C:\\myLocal.reg");
+        }
+
+        static public void ExportReg(string RegKey, string SavePath)
+        {
+            string path = "\"" + SavePath + "\"";
+            string key = "\"" + RegKey + "\"";
+
+            var proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc = Process.Start("regedit.exe", "/e " + path + " " + key + "");
+
+                if (proc != null) proc.WaitForExit();
+            }
+            finally
+            {
+                if (proc != null) proc.Dispose();
+            }
+
+        }
+    }
+}
