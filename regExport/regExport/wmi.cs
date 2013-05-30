8394da978159585fa0bc704163770d60b94f4fe2
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-05-30 17:19:46 +0000

    add new class wmi

---
+++ regExport/regExport/wmi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+namespace regExport
+{
+    class wmi
+    {
+        static public void ExportReg(string RegKey, string SavePath, string ServerName)
+        {
+            string path = "\"" + SavePath + "\"";
+            string key = "\"" + RegKey + "\"";
+            object[] theProcessToRun = { "regedit.exe /e " + path + " " + key + "" };
+            ManagementClass mClass = new ManagementClass(@"\\" + ServerName + @"\root\cimv2:Win32_Process");
+            mClass.InvokeMethod("Create", theProcessToRun);
+        }
+    }
+}
