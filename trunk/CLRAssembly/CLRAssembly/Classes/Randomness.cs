20aea509fd01ce6dfc4c6ba288e5c793ce8b710e
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2013-08-15 17:38:46 +0000

    Add a simple CLR assembly

---
+++ trunk/CLRAssembly/CLRAssembly/Classes/Randomness.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CLRAssembly
+{
+    public sealed class Randomness
+    {
+        public static string Date()
+        {
+            try
+            {
+                DateTime start = new DateTime(1950, 1, 1);
+                Random gen = new Random();
+                int range = (DateTime.Today - start).Days;
+                return start.AddDays(gen.Next(range)).ToString("MM/dd/yyyy");
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public static string Time()
+        {
+            try
+            {
+                DateTime start = new DateTime(1950, 1, 1);
+                Random gen = new Random();
+                return start.AddSeconds(gen.Next(86400)).ToString("HH:mm:ss");
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
