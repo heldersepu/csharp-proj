3582ff86bb51624e492cb5a04ffcac52ebc7f2f3
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-01-09 15:17:07 +0000

    Add a new project: geom it is a console application to address issues on POLYGON with Ring Self-intersection

---
+++ geom/geom/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace geom
+{
+    class Program
+    {
+        static string clean_intersections(string polygon)
+        {
+            return polygon;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.BufferHeight = 2400;
+            Console.BufferWidth = 120;
+            Console.WindowWidth = 120;
+            Console.ForegroundColor = ConsoleColor.Red; 
+            Console.WriteLine("");
+            Console.WriteLine("  --- geom ---  ");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Press Enter to proceed...");            
+            Console.ReadLine();
+
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            foreach (string poly in util.polygons)
+            {
+                Console.ForegroundColor++;
+                string cleanPoly = clean_intersections(poly);
+                Console.WriteLine(cleanPoly);
+                Console.WriteLine("");
+                
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+        }        
+    }
+}
