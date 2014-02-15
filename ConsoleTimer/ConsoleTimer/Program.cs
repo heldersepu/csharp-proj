2332dd5999cc44ed0a0bea04175c81b11e7958df
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-02-15 21:37:03 +0000

    simple console function to show a down counting timer

---
+++ ConsoleTimer/ConsoleTimer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+namespace ConsoleTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            showTimer(5);
+            showTimer(110);
+        }
+
+        static void showTimer(double TimeInSec)
+        {
+            Console.Write("          ");
+            string outPut = TimeInSec.ToString("F");
+            int max = outPut.Length;
+            string backSpace = new String(Convert.ToChar(8), max);
+            while (TimeInSec > 0)
+            {
+                Console.Write(backSpace);
+                outPut = TimeInSec.ToString("F");
+                Console.Write(new String(' ', max - outPut.Length));
+                Console.Write(outPut);
+                TimeInSec -= 0.01;
+                Thread.Sleep(10);                
+            }
+            Console.Write(backSpace);
+            Console.WriteLine("0.00");
+        }
+    }
+}
