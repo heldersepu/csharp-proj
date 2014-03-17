7fcb62dafb2bd7c6a10665b09cc9dc8cbf1b8550
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-03-17 21:37:24 +0000

    SampleList

---
+++ trunk/SampleList/SampleList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myList abc = new myList()
+            {
+                new node("d", 8),
+                new node("a", 2),
+                new node("c", 6),
+                new node("b", 4)
+            };
+            foreach (node n in abc)
+            {
+                Console.Write(n.value);
+                Console.Write(n.value);
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
+        }
+
+        class myList : List<node>
+        {
+
+        }
+
+        class node
+        {
+            public node(string n, int v)
+            {
+                name = n;
+                value = v;
+            }
+            public string name;
+            public int value;
+        }
+    }
+}
