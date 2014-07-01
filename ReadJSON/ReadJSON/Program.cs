564a03a465ed7bae79319ed38d6894ba9b5338cc
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-07-01 16:07:42 +0000

    Using Newtonsoft to parse JSON strings

---
+++ ReadJSON/ReadJSON/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+
+namespace ReadJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                foreach (string arg in args)
+                {
+                    string url = readUrl(arg);
+                    parseJSON.dynamic(url);
+                }
+                Console.ReadLine();
+            }
+        }
+
+        static string readUrl(string url)
+        {
+            string s = "";
+            using (WebClient client = new WebClient())
+            {
+                s = client.DownloadString(url);
+            }
+            return s;
+        }
+
+       
+    }
+}
---
+++ ReadJSON/ReadJSON/parseJSON.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ReadJSON
+{
+    class parseJSON
+    {
+        public static void reader(string json)
+        {
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+            while (reader.Read())
+            {
+                if (reader.Value != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Token: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(reader.TokenType);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(", Value: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(reader.Value);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Token: {0}", reader.TokenType);
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+        }
+
+        public static void dynamic(string json)
+        {
+            dynamic dJSON = JsonConvert.DeserializeObject(json);
+            if (dJSON.Type == JTokenType.Array)
+            {
+                foreach (dynamic jNode in dJSON)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Name: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(jNode.name);
+                }
+            }
+
+        }
+
+    }
+}
