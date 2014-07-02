dab51f92883b1692f51f0e792627121cbca1bdce
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-07-02 16:16:32 +0000

    Continue work on the ToTango class

--- trunk/absToTango/absToTango/Program.cs
+++ trunk/absToTango/absToTango/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace absToTango
 {
@@ -9,6 +6,27 @@ namespace absToTango
     {
         static void Main(string[] args)
         {
+            dynamic account = null;
+            string token = "";
+            string url = "https://app.totango.com/api/v1/accounts/active_list/1/current.json?return=stats";
+            using (ToTangoReader tangoReader = new ToTangoReader(token, url))
+            {
+                do
+                {
+                    account = tangoReader.ReadAccount();
+                    if (account != null)
+                    {
+                        Console.WriteLine(account.name);
+                        Console.WriteLine(account.account_id);
+                    } else if (tangoReader.Error) {
+                        Console.WriteLine(tangoReader.ErrorMessage);
+                    }
+                }
+                while (account != null);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
         }
     }
 }
--- trunk/absToTango/absToTango/ToTango.cs
+++ trunk/absToTango/absToTango/ToTango.cs
@@ -2,58 +2,122 @@
 using System.Net;
 using Newtonsoft.Json;
 
-
 namespace absToTango
 {
-    class ToTangoReader
+    /// <summary>
+    /// Provides a simple abstraction layer to the ToTango API, making the pagination transparent.
+    /// </summary>
+    class ToTangoReader : IDisposable
     {
         private string _token;
-        private string _url;
-        private Int64 _pos;
-        private Int64 _length;
-        private Int64 _offset;
-        private Int64 _total_items;
-        private Int64 _item_count;
+        private string _url = "";
+        private string _err = "";
+        private Int64 _pos = 0;
+        private Int64 _length = 0;
+        private Int64 _offset = 0;
+        private Int64 _total_items = 0;
+        private Int64 _item_count = 0;
+        private Int64 _global_item_count = 0;
         private dynamic _dJSON;
 
-        public ToTangoReader(string token, string url, int length)
+        /// <summary>
+        /// Initializes a new instance of the ToTangoReader class.
+        /// </summary>
+        /// <param name="token">Your ToTango API authentication key.</param>
+        /// <param name="url">The Url to the API, something like: https://app.totango.com/api/v1/accounts/active_list/1/current.json?return=stats</param>
+        /// <param name="length">Optional parameter default to 100.</param>
+        public ToTangoReader(string token, string url, int length = 100)
         {
             this._token = token;
-            this._url = url;
-            this._pos = 0;
+            this._url = url.Trim();
             this._length = length;
-            this._offset = 0;
-            this._item_count = 0;
             this.initJSON();
+        }
+
+        /// <summary>
+        ///  Gets whether there was an error
+        /// </summary>
+        public bool Error
+        {
+            get { return (_err != ""); }
+        }
+
+        /// <summary>
+        ///  Shows the error message if any
+        /// </summary>
+        public string ErrorMessage
+        {
+            get { return _err; }
         }
 
         private void initJSON()
         {
-            string url = this._url + "length=" + this._length + "&offset=" + this._offset;
-            string json = "";
-            using (WebClient client = new WebClient())
+            if (this._url != "")
             {
-                client.Headers.Add("Authorization", this._token);
-                json = client.DownloadString(url);
+                string url = this._url + "length=" + this._length + "&offset=" + this._offset;
+                string json = "";
+                using (WebClient client = new WebClient())
+                {
+                    try
+                    {
+                        client.Headers.Add("Authorization", this._token);
+                        json = client.DownloadString(url);
+                    }
+                    catch (Exception e)
+                    {
+                        this._err = e.Message;
+                    }
+                }
+                if (this._err == "")
+                {
+                    this._dJSON = JsonConvert.DeserializeObject(json);
+                    this._item_count = (int)this._dJSON.current_item_count;
+                    this._total_items = (int)this._dJSON.total_items;
+                    this._global_item_count += this._global_item_count;
+                }
             }
-            this._dJSON = JsonConvert.DeserializeObject(json);
-            this._item_count = (int)this._dJSON.current_item_count;
-            this._total_items = (int)this._dJSON.total_items;
-
         }
 
+        /// <summary>
+        /// Reads the next account from the underlying stream and advances the current position.
+        /// </summary>
+        /// <returns>The next account from the stream, or null if no account are available.</returns>
         public dynamic ReadAccount()
         {
             dynamic dAccount = null;
             try
             {
-                dAccount = this._dJSON.accounts[this._pos++];
+                if (this._global_item_count < this._total_items)
+                {
+                    if (this._item_count == this._pos)
+                    {
+                        this._offset++;
+                        this.initJSON();
+                        this._pos = 0;
+                    }
+                    dAccount = this._dJSON.accounts[this._pos++];
+                }
             }
-            catch (Exception e) 
+            catch (Exception e)
             {
                 dAccount = null;
             }
             return dAccount;
+        }
+
+        /// <summary>
+        /// Releases all resources used by the current instance of the class
+        /// </summary>
+        public void Dispose()
+        {
+            this._url = "";
+            this._pos = 0;
+            this._length = 0;
+            this._offset = 0;
+            this._total_items = 0;
+            this._item_count = 0;
+            this._global_item_count = 0;
+            this._dJSON = null;
         }
     }
 }
