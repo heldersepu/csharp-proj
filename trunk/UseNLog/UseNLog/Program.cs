61ff16e0f6d99d4e84b8e8d2e3cd4ae8a0c9207d
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-08-07 16:24:10 +0000

    Sample Project using NLog

---
+++ trunk/UseNLog/UseNLog/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NLog;
+
+namespace UseNLog
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin");
+            
+            logger.Info("Begin");
+            logger.Error("Bad error");
+            
+            try
+            {
+                throw new DivideByZeroException();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+
+            try
+            {
+                raiseErr();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+            }
+
+            logger.Info("End");
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+        }
+
+        static void raiseErr()
+        {
+            throw new BadImageFormatException();
+        }
+    }
+}
