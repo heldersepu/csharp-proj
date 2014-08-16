85876f9c8b9008abadab98cc57e46d10122eebd3
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-08-16 05:12:27 +0000

    Add a new class to handle exports

--- trunk/ToTango/absToTango/absToTango/Program.cs
+++ trunk/ToTango/absToTango/absToTango/Program.cs
@@ -5,9 +5,34 @@ namespace absToTango
     class Program
     {
         static void Main(string[] args)
+        {            
+            string token = "ab63b0d7e591ff0b8209ef9358478c4876c29787heldersepu@gmail.com";
+
+            if (args.Length == 0)
+            {
+                TestReader(token);
+            }
+            else
+            {
+                TestExport(token);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+        }
+
+        static void TestExport(string token)
+        {
+            string url = "https://app.totango.com/api/v1/accounts/active_list/10010/current.json";
+            var tangoXport = new ToTangoExport(token);
+            tangoXport.Start(url);
+        }
+
+        static void TestReader(string token)
         {
             dynamic account = null;
-            string token = "";
             string url = "https://app.totango.com/api/v1/accounts/active_list/1/current.json?return=stats";
             using (ToTangoReader tangoReader = new ToTangoReader(token, url))
             {
@@ -20,16 +45,14 @@ namespace absToTango
                         Console.Write(account.name + " ");
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine(account.totango_id);
-                    } else if (tangoReader.Error) {
+                    }
+                    else if (tangoReader.Error)
+                    {
                         Console.WriteLine(tangoReader.ErrorMessage);
                     }
                 }
                 while (account != null);
             }
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("");
-            Console.WriteLine("Press ENTER to continue...");
-            Console.ReadLine();
         }
     }
 }
---
+++ trunk/ToTango/absToTango/absToTango/ToTangoExport.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace absToTango
+{
+    /// <summary>
+    /// Provides an easy way to export ToTango filters 
+    /// </summary>
+    class ToTangoExport
+    {
+        private string _token;
+        /// <summary>
+        /// Initializes a new instance of the ToTangoExport class.
+        /// </summary>
+        /// <param name="token">Your ToTango API authentication key.</param>
+        
+        public ToTangoExport(string token)
+        {
+            this._token = token;
+        }
+
+        /// <summary>
+        /// Start the export on the given url
+        /// </summary>
+        /// <param name="url">The Url to the API, something like: https://app.totango.com/api/v1/accounts/active_list/10010/current.json</param>        
+        public void Start(string url)
+        {
+            dynamic account = null;
+            using (ToTangoReader tangoReader = new ToTangoReader(this._token, url))
+            {
+                do
+                {
+                    account = tangoReader.ReadAccount();
+                    if (account != null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write(account.totango_id);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(" " + getAttrib(account, "MemberFullName"));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(" " + getAttrib(account, "MemberPhone"));
+                    }
+                    else if (tangoReader.Error)
+                    {
+                        Console.WriteLine(tangoReader.ErrorMessage);
+                    }
+                }
+                while (account != null);
+            }
+        }
+
+        private string getAttrib(dynamic account, string attrib)
+        {
+            string ret = "";
+            if (account.attributes[attrib] != null)
+            {
+                ret = account.attributes[attrib].value;
+            }
+            return ret;
+        }
+    }
+}
