80714c29230d73db952004ed2e347acf1d1fd465
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-08-16 19:51:34 +0000

    Improve the export capabilities

--- trunk/ToTango/absToTango/absToTango/ToTangoExport.cs
+++ trunk/ToTango/absToTango/absToTango/ToTangoExport.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
 namespace absToTango
 {
@@ -11,49 +12,72 @@ namespace absToTango
     class ToTangoExport
     {
         private string _token;
+        private string _headers = "";
+        private string _aliasHead = "";
+
         /// <summary>
         /// Initializes a new instance of the ToTangoExport class.
         /// </summary>
-        /// <param name="token">Your ToTango API authentication key.</param>
-        
-        public ToTangoExport(string token)
-        {
+        /// <param name="token">Your ToTango API authentication key.</param>        
+        /// <param name="headerFile">Your Mapping file.</param>
+        public ToTangoExport(string token, string headerFile)
+        {            
             this._token = token;
+            foreach (string line in File.ReadAllLines(headerFile))
+            {
+                if ((line.Trim().Length > 0) && (!line.StartsWith("#")))
+                {
+                    if (this._headers == "")
+                    {
+                        this._headers = line.Replace("\t", "");
+                        this._aliasHead = this._headers;
+                    }
+                    else
+                    {
+                        this._aliasHead = line.Replace("\t", "");
+                        break;
+                    }
+                }
+            }
         }
 
         /// <summary>
         /// Start the export on the given url
         /// </summary>
         /// <param name="url">The Url to the API, something like: https://app.totango.com/api/v1/accounts/active_list/10010/current.json</param>        
-        public void Start(string url)
+        /// <param name="outname">The name of the output file</param> 
+        public void Start(string url, string outname)
         {
-            dynamic account = null;
-            using (ToTangoReader tangoReader = new ToTangoReader(this._token, url))
+            List<string> lines = new List<string>();
+            lines.Add(this._aliasHead);
+            if (this._headers.Length > 0)
             {
-                do
+                using (ToTangoReader tangoReader = new ToTangoReader(this._token, url))
                 {
-                    account = tangoReader.ReadAccount();
-                    if (account != null)
+                    dynamic account = null;
+                    do
                     {
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write(account.totango_id);
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(" " + getAttrib(account, "MemberFullName"));
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine(" " + getAttrib(account, "MemberPhone"));
+                        account = tangoReader.ReadAccount();
+                        if (account != null)
+                        {
+                            string line = "";
+                            foreach (string attrib in this._headers.Split(','))
+                            {
+                                line += getAttrib(account, attrib) + ",";
+                            }
+                            lines.Add(line.TrimEnd(','));
+                        }
                     }
-                    else if (tangoReader.Error)
-                    {
-                        Console.WriteLine(tangoReader.ErrorMessage);
-                    }
+                    while (account != null);
                 }
-                while (account != null);
             }
+            File.WriteAllLines(outname, lines);
         }
 
         private string getAttrib(dynamic account, string attrib)
         {
             string ret = "";
+            attrib = attrib.Trim();
             if (account.attributes[attrib] != null)
             {
                 ret = account.attributes[attrib].value;
