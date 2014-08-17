0823f9be37029aad99ce9ceb11b6f6d5f818d7ec
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-08-17 02:46:38 +0000

    final work on the installer

--- trunk/ToTango/xInstall/build.bat
+++ trunk/ToTango/xInstall/build.bat
@@ -1,15 +1,28 @@
-@SET idir=F:\csharp-proj\ToTango
 @SET DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"
 
-@RMDIR /S /Q %idir%\absToTango\absToTango\bin
-@RMDIR /S /Q %idir%\absToTango\absToTango\obj
-@RMDIR /S /Q %idir%\ToTangoXport\ToTangoXport\bin
-@RMDIR /S /Q %idir%\ToTangoXport\ToTangoXport\obj
+@COPY setup.nsi ..
+@DEL *.exe
+@CD ..
+@DEL *.exe
+@RMDIR /S /Q absToTango\absToTango\bin
+@RMDIR /S /Q absToTango\absToTango\obj
+@RMDIR /S /Q ToTangoXport\ToTangoXport\bin
+@RMDIR /S /Q ToTangoXport\ToTangoXport\obj
 
-%DEVENV% %idir%\absToTango\absToTango.sln /Build
-%DEVENV% %idir%\ToTangoXport\ToTangoXport.sln /Build
-COPY  %idir%\absToTango\absToTango\mapping.csv %idir%\ToTangoXport\ToTangoXport\bin\Debug
+%DEVENV% absToTango\absToTango.sln /Build
+%DEVENV% ToTangoXport\ToTangoXport.sln /Build
+COPY  absToTango\absToTango\mapping.csv 			ToTangoXport\ToTangoXport\bin\Debug
+COPY  ToTangoXport\ToTangoXport\sample.ToTango 		ToTangoXport\ToTangoXport\bin\Debug
 
 @ECHO.
-@DEL /F /S %idir%\*.pdb 
+@DEL /F /S *.pdb 
 @PING 127.0.0.1 > nul
+
+@COLOR 07
+SET NSIS="%ProgramFiles%\NSIS\makensis.exe"
+IF NOT EXIST %NSIS% SET NSIS="%ProgramFiles(x86)%\NSIS\makensis.exe"
+CALL %NSIS% setup.nsi
+@ECHO.
+@PING 127.0.0.1 > nul
+@MOVE *.exe xInstall
+DEL setup.nsi
