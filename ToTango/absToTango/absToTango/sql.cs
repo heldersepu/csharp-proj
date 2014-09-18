ced8ab58d53b413579dbcdaaa1d90e7bd51dacdb
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-09-18 19:22:45 +0000

    start some logic to save data to SQL

---
+++ ToTango/absToTango/absToTango/sql.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace absToTango
+{
+    class sql
+    {
+
+        private string _sqlConn = "";
+
+        /// <summary>
+        /// Initializes a new instance of the sql class
+        /// </summary>
+        /// <param name="sqlConString">SQL connection string</param>
+        public sql(string sqlConString)
+        {
+            this._sqlConn = sqlConString;
+        }
+
+        public Int64 addCall()
+        {
+            return 0;
+        }
+
+        public void addAttrib(Int64 id, string key, string value)
+        {
+            
+        }
+    }
+}
