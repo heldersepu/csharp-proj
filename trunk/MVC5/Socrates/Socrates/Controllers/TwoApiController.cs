fd8107c0efc68cc6aeadcc07b0c32492cec95909
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-12-05 20:10:16 +0000

    Add webApi controllers

---
+++ trunk/MVC5/Socrates/Socrates/Controllers/TwoApiController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Socrates.Models;
+
+namespace Socrates.Controllers
+{
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class TwoApiController : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+        
+        public IHttpActionResult Get(string id)
+        {
+            var crs = products.ToList(); // FirstOrDefault((p) => p.Id == id);
+            if (crs == null)
+            {
+                return NotFound();
+            }
+            return Ok(crs);
+        }
+    }
+}
