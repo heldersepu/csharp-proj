7b94c5f4e1056843d666db6ace66ae3bb2f35db6
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-12-05 16:18:58 +0000

    Add fancybox

--- trunk/MVC5/Socrates/Socrates/App_Start/BundleConfig.cs
+++ trunk/MVC5/Socrates/Socrates/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Socrates
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.fancybox.pack.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,6 +26,7 @@ namespace Socrates
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery.fancybox.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
