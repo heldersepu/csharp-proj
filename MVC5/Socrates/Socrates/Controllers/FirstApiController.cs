25de3e4bd9f2881bc5ceca771a00990adb0ae637
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-12-05 20:10:16 +0000

    Add webApi controllers

---
+++ MVC5/Socrates/Socrates/Controllers/FirstApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Socrates.Controllers
+{
+    public class FirstApiController : ApiController
+    {
+        public string Get(string id)
+        {
+            return "Hello World " + id;
+        }
+    }
+}
---
+++ MVC5/Socrates/Socrates/Controllers/OneApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Socrates.Models;
+
+namespace Socrates.Controllers
+{
+
+    public class OneApiController : ApiController
+    {
+
+        public IHttpActionResult Get(int id)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            var context = new SocratesContext(connStr);
+            var crs = context.Courses.ToList();
+
+            if (crs == null)
+            {
+                return NotFound();
+            }
+            return Ok(crs);
+        }
+    }
+}
