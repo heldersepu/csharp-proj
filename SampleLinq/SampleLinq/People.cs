5f429398e001e82494592d038700b19589991b01
heldersepu <heldersepu@caeeb5fc-eaae-407d-990c-75020bcee741>
2014-12-13 16:31:58 +0000

    Move the classes to own files

---
+++ SampleLinq/SampleLinq/People.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleLinq
+{
+    class People : List<Person>
+    {
+        /** USE LINQ for the following 2 Methods **/
+
+        public People Originals()
+        {
+            /** Return a list of unique based on Fname & Lname **/
+            return this;
+        }
+
+        public People Duplicates()
+        {
+            /** Return a list of duplicates **/
+            return this;
+        }
+    }
+}
--- SampleLinq/SampleLinq/Program.cs
+++ SampleLinq/SampleLinq/Program.cs
@@ -1,84 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SampleLinq
 {
     class Program
     {
-        class Person
-        {
-            public string Fname;
-            public string Lname;
-            public string Address;
-            public string Phone;
-
-            public Person(string name, string lname, string address, string phone)
-            {
-                this.Fname = name;
-                this.Lname = lname;
-                this.Address = address;
-                this.Phone = phone;
-            }
-
-            public override string ToString()
-            {
-                return this.Fname + " " + this.Lname + " Address:" + this.Address + " Phone:" + this.Phone;
-            }
-        }
-
-        class People : List<Person>
-        {
-            /** USE LINQ for the following 2 Methods **/
-
-            public List<Person> Originals()
-            {
-                /** Return a list of unique based on Fname & Lname **/
-                return this;
-            }
-
-            public List<Person> Duplicates()
-            {
-                /** Return a list of duplicates **/
-                return this;
-            }
-        }
-
         static void Main(string[] args)
         {
             People people = new People() 
             {
-                new Person("John", "Doe", "132 Dr",   "305 456 7899"),
-                new Person("Jane", "Doe", "546 Road", "786 789 8787"),
-                new Person("John", "Doe", "789 Ave",  "954 354 7982")
+                new Person("John", "Doe",  "132 Dr",    "305 456 7899"),
+                new Person("Jane", "Doe",  "546 Road",  "786 789 8787"),
+                new Person("John", "Doe",  "789 Ave",   "954 354 7982"),
+                new Person("Jack", "Doe",  "467 St",    "954 342 7111"),
+                new Person("John", "Deer", "123 Blvd",  "542 234 7982"),
             };
 
             Console.ForegroundColor = ConsoleColor.White;
+            showList("  Entire List", people);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            showList("  Originals", people.Originals());
+            
+            Console.ForegroundColor = ConsoleColor.Red;
+            showList("  Duplicates", people.Duplicates());
+
             Console.WriteLine("");
-            Console.WriteLine("Entire List");            
+            Console.ReadLine();
+        }
+
+        static void showList(string message, People people)
+        {            
+            Console.WriteLine("");
+            Console.WriteLine(message);
             foreach (Person person in people)
             {
                 Console.WriteLine(person);
             }
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("");
-            Console.WriteLine("Originals");
-            foreach (Person person in people.Originals())
-            {
-                Console.WriteLine(person);
-            }
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("");
-            Console.WriteLine("Duplicates");
-            foreach (Person person in people.Duplicates())
-            {
-                Console.WriteLine(person);
-            }
-
-            Console.ReadLine();
         }
     }
 }
