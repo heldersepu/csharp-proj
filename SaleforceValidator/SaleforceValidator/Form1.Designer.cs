e0134e7c5d6dacab81f63607b20fb4f1ef42264d
HelderSepu <HelderSepu@gmail.com>
2015-12-07 14:40:55 -0500

    New project to validate SalesForce API credentials

---
+++ SaleforceValidator/SaleforceValidator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace SaleforceValidator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_Secret = new System.Windows.Forms.TextBox();
+            this.textBox_User = new System.Windows.Forms.TextBox();
+            this.textBox_Passw = new System.Windows.Forms.TextBox();
+            this.textBox_Token = new System.Windows.Forms.TextBox();
+            this.textBox_Domain = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.textBox_Resp = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Key.Location = new System.Drawing.Point(100, 26);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(226, 20);
+            this.textBox_Key.TabIndex = 0;
+            // 
+            // textBox_Secret
+            // 
+            this.textBox_Secret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Secret.Location = new System.Drawing.Point(100, 52);
+            this.textBox_Secret.Name = "textBox_Secret";
+            this.textBox_Secret.Size = new System.Drawing.Size(226, 20);
+            this.textBox_Secret.TabIndex = 1;
+            // 
+            // textBox_User
+            // 
+            this.textBox_User.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_User.Location = new System.Drawing.Point(100, 78);
+            this.textBox_User.Name = "textBox_User";
+            this.textBox_User.Size = new System.Drawing.Size(226, 20);
+            this.textBox_User.TabIndex = 2;
+            // 
+            // textBox_Passw
+            // 
+            this.textBox_Passw.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Passw.Location = new System.Drawing.Point(100, 104);
+            this.textBox_Passw.Name = "textBox_Passw";
+            this.textBox_Passw.Size = new System.Drawing.Size(226, 20);
+            this.textBox_Passw.TabIndex = 3;
+            // 
+            // textBox_Token
+            // 
+            this.textBox_Token.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Token.Location = new System.Drawing.Point(100, 130);
+            this.textBox_Token.Name = "textBox_Token";
+            this.textBox_Token.Size = new System.Drawing.Size(226, 20);
+            this.textBox_Token.TabIndex = 4;
+            // 
+            // textBox_Domain
+            // 
+            this.textBox_Domain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Domain.Location = new System.Drawing.Point(100, 156);
+            this.textBox_Domain.Name = "textBox_Domain";
+            this.textBox_Domain.Size = new System.Drawing.Size(226, 20);
+            this.textBox_Domain.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Secret";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Passw";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Token";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Api Domain";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(140, 226);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 12;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // textBox_Resp
+            // 
+            this.textBox_Resp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Resp.Location = new System.Drawing.Point(22, 12);
+            this.textBox_Resp.Multiline = true;
+            this.textBox_Resp.Name = "textBox_Resp";
+            this.textBox_Resp.Size = new System.Drawing.Size(317, 186);
+            this.textBox_Resp.TabIndex = 13;
+            this.textBox_Resp.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 279);
+            this.Controls.Add(this.textBox_Resp);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Domain);
+            this.Controls.Add(this.textBox_Token);
+            this.Controls.Add(this.textBox_Passw);
+            this.Controls.Add(this.textBox_User);
+            this.Controls.Add(this.textBox_Secret);
+            this.Controls.Add(this.textBox_Key);
+            this.Name = "Form1";
+            this.Text = "Salesforce Validator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_Secret;
+        private System.Windows.Forms.TextBox textBox_User;
+        private System.Windows.Forms.TextBox textBox_Passw;
+        private System.Windows.Forms.TextBox textBox_Token;
+        private System.Windows.Forms.TextBox textBox_Domain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox textBox_Resp;
+    }
+}
+
