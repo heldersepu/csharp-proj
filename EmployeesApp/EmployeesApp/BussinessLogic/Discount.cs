2413583828a3d1e61575dcc5df77c46b7468cf2b
HelderSepu <heldersepu@gmail.com>
2016-01-22 14:47:58 -0500

    Validate name is not null or blank

--- EmployeesApp/EmployeesApp/BussinessLogic/Discount.cs
+++ EmployeesApp/EmployeesApp/BussinessLogic/Discount.cs
@@ -10,25 +10,28 @@ namespace EmployeesApp.BussinessLogic
         /// <summary>
         /// Logic for the Benefits discount
         /// </summary>
-        /// <param name="Name">Name of person cheking for discounts</param>
+        /// <param name="Name">Name of person (Employee or Dependent)</param>
         /// <returns>Returns discount percentage</returns>
         public static double Benefits(string Name)
         {
             double percentage = 0;
-            var Discounts = new BenefitsDiscountsController().Get();
-            foreach (var discount in Discounts)
+            if (!string.IsNullOrEmpty(Name))
             {
-                switch (discount.Type)
+                var Discounts = new BenefitsDiscountsController().Get();
+                foreach (var discount in Discounts)
                 {
-                    case "StartsWith":
-                        percentage = (Name.StartsWith(discount.Value)) ? discount.Percentage : 0;
-                        break;
-                    case "EndsWith":
-                        percentage = (Name.EndsWith(discount.Value)) ? discount.Percentage : 0;
-                        break;
-                    case "Equals":
-                        percentage = (Name.Equals(discount.Value)) ? discount.Percentage : 0;
-                        break;
+                    switch (discount.Type)
+                    {
+                        case "StartsWith":
+                            percentage = (Name.StartsWith(discount.Value)) ? discount.Percentage : 0;
+                            break;
+                        case "EndsWith":
+                            percentage = (Name.EndsWith(discount.Value)) ? discount.Percentage : 0;
+                            break;
+                        case "Equals":
+                            percentage = (Name.Equals(discount.Value)) ? discount.Percentage : 0;
+                            break;
+                    }
                 }
             }
             return percentage;
