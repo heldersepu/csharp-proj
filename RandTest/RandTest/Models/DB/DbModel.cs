7faa343ebb3877f5d30cffab5b284f4751f1884f
HelderSepu <heldersepu@gmail.com>
2016-02-07 19:10:37 -0500

    Add RandTest

---
+++ RandTest/RandTest/Models/DB/DbModel.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+using System.Data.Entity;
+
+namespace RandTest.Models
+{
+    public class DbModel : DbContext
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public DbModel() : base("name=RandTest")
+        {
+            this.Database.Log = x => logger.Trace(x);
+        }
+
+        public virtual DbSet<Test> Tests { get; set; }
+        public virtual DbSet<Question> Questions { get; set; }
+        public virtual DbSet<Choice> Choices { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Test>()
+                .HasMany(x => x.Questions)
+                .WithRequired(x => x.Test)
+                .WillCascadeOnDelete(true);
+
+            modelBuilder.Entity<Question>()
+                .HasMany(x => x.Choices)
+                .WithRequired(x => x.Question)
+                .WillCascadeOnDelete(true);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ RandTest/RandTest/UI/test.js
@@ -0,0 +1,57 @@
+﻿$(window).load(function () {    
+    loadTest();
+    $("#btnSubmit").click(doSubmit);
+});
+
+function loadTest() {
+    $("#loading").show();
+    $("#divTable").hide()
+    $.ajax({
+        type: "GET",
+        url: "/api/Test",
+        dataType: "json",
+        cache: false,
+        success: createTable,
+        error: errorFunc,
+    });
+}
+
+function doSubmit() {
+    alert("Work in progress...")
+}
+
+function createTable(test) {
+    if ($("#testTable tr").length > 0) {
+        $("#testTable").DataTable().destroy();
+    }
+    $("#testTable").empty();
+    var row = "<tr><th>Questions</th></tr>";
+    $("#testTable").append("<thead>" + row + "</thead>");
+    $("#testName").html(test.Name);
+    for (var i = 0; i < test.Questions.length; i++) {
+        var row = "<tr><td>" + (i + 1) + ". " + test.Questions[i].Detail + "";
+        var type = (test.Questions[i].SingleAnswer ? "radio" : "checkbox");
+        for (var j = 0; j < test.Questions[i].Choices.length; j++) {
+            var choice = test.Questions[i].Choices[j]            
+            row += "<br><input type=" + type + " name=check" + i + " value=" + choice.Id + ">"
+            row += "<span >" + choice.Detail + "</span>"
+        }
+        row += "</td><tr>";
+        $("#testTable").append(row);
+    }
+    $("#loading").hide();
+    $("#divTable").show();
+    $("#testTable").DataTable( {
+        "paging": false,
+        "searching": false,
+        "ordering": false,
+        "info": false
+    });    
+}
+
+function errorFunc(err) {
+    if (err.responseText == "")
+        alert("Sorry the communication failed.");
+    else
+        alert(err.responseText);
+}
