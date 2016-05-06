1c20753d8c1736a49a9bd7df6349947eac8d72b9
HelderSepu <HelderSepu@gmail.com>
2016-05-06 09:28:51 -0400

    Add JsonDeserialize solution

---
+++ JsonDeserialize/JsonDeserialize/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace JsonDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = UseNewtonsoft(@"Account.json");
+            Console.WriteLine(account.Email);
+        }
+
+        static Account UseNewtonsoft(string file)
+        {
+            string json = File.ReadAllText(file, Encoding.UTF8); ;
+            return JsonConvert.DeserializeObject<Account>(json);            
+        }
+    }
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+        public Dictionary<int,string> Locations { get; set; }
+    }
+}
