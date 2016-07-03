d8efbdacaf40585d77a68bdea4c061279c0387d8
HelderSepu <heldersepu@gmail.com>
2016-07-03 15:13:46 -0400

    Refactor the BikeDistributor solution - Use dynamic Linq for the Discount - Add templates for the receipts - Add unit tests for the discounts

--- bike-distributor-refactor/BikeDistributor/Line.cs
+++ bike-distributor-refactor/BikeDistributor/Line.cs
@@ -1,7 +1,18 @@
-﻿namespace BikeDistributor
+﻿using System.Linq.Dynamic;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using NLog;
+
+namespace BikeDistributor
 {
+    /// <summary>
+    /// A bicycle line 
+    /// </summary>
     public class Line
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public Line(Bike bike, int quantity)
         {
             Bike = bike;
@@ -10,5 +21,41 @@
 
         public Bike Bike { get; private set; }
         public int Quantity { get; private set; }
+        public DateTime DateTimeNow
+        {
+            get { return DateTime.Now; }
+        }
+
+        /// <summary>
+        /// Calculate the cost for the entire line
+        /// </summary>
+        /// <param name="discounts">list of discounts</param>
+        /// <returns>Returns total cost for the line</returns>
+        public double Cost(List<Discount> discounts)
+        {
+            double thisAmount = Quantity * Bike.Price;
+            if (discounts != null)
+            {
+                var obj = new List<Line> { this };
+                foreach (var discount in discounts.OrderBy(x => x.Id))
+                {
+                    try
+                    {
+                        if (discount.Percentage > 0
+                            && !string.IsNullOrEmpty(discount.Condition) 
+                            && obj.Where(discount.Condition).Any())
+                        {
+                            thisAmount *= discount.Percentage;
+                            break;
+                        }
+                    }
+                    catch (System.Exception e)
+                    {
+                        logger.Error(e);
+                    }                    
+                }
+            }
+            return thisAmount;
+        }
     }
 }
