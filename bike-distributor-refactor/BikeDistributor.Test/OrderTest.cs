2b9d9b9fa2e6cb557b80f34c55eac02f6e5c112c
HelderSepu <heldersepu@gmail.com>
2016-07-03 15:10:33 -0400

    Add BikeDistributor solution

---
+++ bike-distributor-refactor/BikeDistributor.Test/OrderTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BikeDistributor.Test
+{
+    [TestClass]
+    public class OrderTest
+    {
+        private readonly static Bike Defy = new Bike("Giant", "Defy 1", Bike.OneThousand);
+        private readonly static Bike Elite = new Bike("Specialized", "Venge Elite", Bike.TwoThousand);
+        private readonly static Bike DuraAce = new Bike("Specialized", "S-Works Venge Dura-Ace", Bike.FiveThousand);
+
+        [TestMethod]
+        public void ReceiptOneDefy()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(Defy, 1));
+            Assert.AreEqual(ResultStatementOneDefy, order.Receipt());
+        }
+
+        private const string ResultStatementOneDefy = @"Order Receipt for Anywhere Bike Shop
+	1 x Giant Defy 1 = $1,000.00
+Sub-Total: $1,000.00
+Tax: $72.50
+Total: $1,072.50";
+
+        [TestMethod]
+        public void ReceiptOneElite()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(Elite, 1));
+            Assert.AreEqual(ResultStatementOneElite, order.Receipt());
+        }
+
+        private const string ResultStatementOneElite = @"Order Receipt for Anywhere Bike Shop
+	1 x Specialized Venge Elite = $2,000.00
+Sub-Total: $2,000.00
+Tax: $145.00
+Total: $2,145.00";
+
+        [TestMethod]
+        public void ReceiptOneDuraAce()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(DuraAce, 1));
+            Assert.AreEqual(ResultStatementOneDuraAce, order.Receipt());
+        }
+
+        private const string ResultStatementOneDuraAce = @"Order Receipt for Anywhere Bike Shop
+	1 x Specialized S-Works Venge Dura-Ace = $5,000.00
+Sub-Total: $5,000.00
+Tax: $362.50
+Total: $5,362.50";
+
+        [TestMethod]
+        public void HtmlReceiptOneDefy()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(Defy, 1));
+            Assert.AreEqual(HtmlResultStatementOneDefy, order.HtmlReceipt());
+        }
+
+        private const string HtmlResultStatementOneDefy = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Giant Defy 1 = $1,000.00</li></ul><h3>Sub-Total: $1,000.00</h3><h3>Tax: $72.50</h3><h2>Total: $1,072.50</h2></body></html>";
+
+        [TestMethod]
+        public void HtmlReceiptOneElite()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(Elite, 1));
+            Assert.AreEqual(HtmlResultStatementOneElite, order.HtmlReceipt());
+        }
+
+        private const string HtmlResultStatementOneElite = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized Venge Elite = $2,000.00</li></ul><h3>Sub-Total: $2,000.00</h3><h3>Tax: $145.00</h3><h2>Total: $2,145.00</h2></body></html>";
+
+        [TestMethod]
+        public void HtmlReceiptOneDuraAce()
+        {
+            var order = new Order("Anywhere Bike Shop");
+            order.AddLine(new Line(DuraAce, 1));
+            Assert.AreEqual(HtmlResultStatementOneDuraAce, order.HtmlReceipt());
+        }
+
+        private const string HtmlResultStatementOneDuraAce = @"<html><body><h1>Order Receipt for Anywhere Bike Shop</h1><ul><li>1 x Specialized S-Works Venge Dura-Ace = $5,000.00</li></ul><h3>Sub-Total: $5,000.00</h3><h3>Tax: $362.50</h3><h2>Total: $5,362.50</h2></body></html>";    
+    }
+}
+
+
---
+++ bike-distributor-refactor/BikeDistributor/Bike.cs
@@ -0,0 +1,20 @@
+﻿namespace BikeDistributor
+{
+    public class Bike
+    {
+        public const int OneThousand = 1000;
+        public const int TwoThousand = 2000;
+        public const int FiveThousand = 5000;
+    
+        public Bike(string brand, string model, int price)
+        {
+            Brand = brand;
+            Model = model;
+            Price = price;
+        }
+
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public int Price { get; set; }
+    }
+}
