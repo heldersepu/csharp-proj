a83efb914e21fb8c908ff8df58ecdd2f696fd566
HelderSepu <HelderSepu@gmail.com>
2016-07-29 10:25:54 -0400

    Move part of the code to own classes Add some random number to the test string

---
+++ HighAndLow/HighAndLow/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace HighAndLow
+{
+    public static class Kata
+    {
+        public static string HighAndLow1(string numbers)
+        {
+            var parsed = numbers.Split().Select(int.Parse);
+            return parsed.Max() + " " + parsed.Min();
+        }
+
+        public static string HighAndLow2(string numbers)
+        {
+            int max, min;
+            var arrNum = numbers.Split();
+            max = min = int.Parse(arrNum[0]);
+            for (var i = 1; i < arrNum.Length; i++)
+            {
+                int num = int.Parse(arrNum[i]);
+                if (num > max)
+                    max = num;
+                else if (num < min)
+                    min = num;
+            }
+            return max.ToString() + " " + min.ToString();
+        }
+    }
+}
--- HighAndLow/HighAndLow/Program.cs
+++ HighAndLow/HighAndLow/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace HighAndLow
 {
@@ -7,7 +6,10 @@ namespace HighAndLow
     {
         static void Main(string[] args)
         {
-            const string test = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6";
+            string test = "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6";
+            var r = new Random();
+            for (var x = 1; x < 50; x++)
+                test += " " + r.Next(-500, 500);
             const int loops = 100000;
             DateTime sTime;
 
@@ -16,14 +18,14 @@ namespace HighAndLow
                 Console.ForegroundColor = ConsoleColor.Red;
                 sTime = DateTime.Now;
                 for (var i = 1; i < loops; i++)
-                    HighAndLow1(test);
+                    Kata.HighAndLow1(test);
                 Console.Write(" Test1: " + sTime.Diff());
                 Console.Write("\t");
 
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 sTime = DateTime.Now;
                 for (var i = 1; i < loops; i++)
-                    HighAndLow2(test);
+                    Kata.HighAndLow2(test);
                 Console.Write(" Test2: " + sTime.Diff());
 
                 Console.WriteLine();
@@ -35,34 +37,6 @@ namespace HighAndLow
             Console.Read();
         }
 
-        public static string HighAndLow1(string numbers)
-        {
-            var parsed = numbers.Split().Select(int.Parse);
-            return parsed.Max() + " " + parsed.Min();
-        }
-
-        public static string HighAndLow2(string numbers)
-        {
-            int max, min;
-            var arrNum = numbers.Split();
-            max = min = int.Parse(arrNum[0]);
-            for (var i = 1; i < arrNum.Length; i++)
-            {
-                int num = int.Parse(arrNum[i]);
-                if (num > max)
-                    max = num;
-                else if (num < min)
-                    min = num;
-            }
-            return max.ToString() + " " + min.ToString();
-        }
-    }
-
-    public static class DateTimeExtension
-    {
-        public static double Diff(this DateTime value)
-        {
-            return Math.Round((DateTime.Now - value).TotalMilliseconds);
-        }
+        
     }
 }
