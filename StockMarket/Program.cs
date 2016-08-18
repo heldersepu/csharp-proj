20b6dee54ff1c113fa0f2d68ca8e05e2874a87aa
HelderSepu <HelderSepu@gmail.com>
2016-08-18 14:17:47 -0400

    Add StockMarket sample app

---
+++ StockMarket/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var StockHistory = new List<int> {
+                12, 14, 14, 15, 16, 14, 12, 10, 11, 12, 13, 14, 10, 11, 12, 17
+            };
+
+            foreach (var stock in StockHistory)
+            {
+                Console.WriteLine(stock);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
