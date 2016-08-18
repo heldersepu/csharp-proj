da556460c4b67c5bfa239635acacf7deea3b609a
HelderSepu <HelderSepu@gmail.com>
2016-08-18 14:28:49 -0400

    Add logic to determine if we should Sell or Buy

--- StockMarket/Program.cs
+++ StockMarket/Program.cs
@@ -11,10 +11,22 @@ namespace StockMarket
                 12, 14, 14, 15, 16, 14, 12, 10, 11, 12, 13, 14, 10, 11, 12, 17
             };
 
-            foreach (var stock in StockHistory)
+            for (int i = 0; i < StockHistory.Count - 2; i++)
             {
-                Console.WriteLine(stock);
-            }
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(StockHistory[i]);
+
+                if (StockHistory[i] > StockHistory[i + 1])
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Sell");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" Buy");
+                }
+            }            
 
             Console.ReadLine();
         }
