c5c29fb3059c6240ede5fb68479a40ad2704c79c
HelderSepu <heldersepu@gmail.com>
2016-09-24 12:05:14 -0400

    Sample conversion from string to int using just primitives

---
+++ IntParse/IntParse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IntParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Console.Write(arg);
+                Console.Write(" = ");
+                Console.WriteLine(IntParse(arg));
+            }
+            Console.Read();
+        }
+
+        static int IntParse(string value)
+        {
+            int count = 0;
+            int factor = 1;
+            for (int i = value.Length -1; i >= 0; i--)
+            {
+                if (value[i] >= '0' && value[i] <= '9')
+                    count += (value[i] - '0') * factor;
+                else
+                    return 0;
+                factor *= 10;
+            }
+            return count;
+        }
+    }
+}
