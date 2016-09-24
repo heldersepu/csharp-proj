9571473481d87a434740fd962637ad9a46d191bd
HelderSepu <heldersepu@gmail.com>
2016-09-23 21:43:39 -0400

    Add a small BitWise example

---
+++ BitWiseOpp/BitWiseOpp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BitWiseOpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            foreach (var arg in args)
+            {
+                if (int.TryParse(arg, out n))
+                {
+                    Console.Write(n);
+                    Console.Write(" = ");
+                    Console.WriteLine(CountBits(n));
+                }
+            }
+            Console.Read();
+        }
+
+        static int CountBits(int value)
+        {
+            int count = 0;
+            int bit = 1;
+            for (int i = 0; i < 8; i++)
+            {
+                if ((bit & value) == bit)
+                    count++;
+                bit <<= 1;
+            }
+            return count;
+        }
+    }
+}
