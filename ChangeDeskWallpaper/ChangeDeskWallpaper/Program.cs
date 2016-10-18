0b2deb2699a0e34c1964c610174c71b852002c25
HelderSepu <HelderSepu@gmail.com>
2016-10-18 09:26:21 -0400

    Remove unused usings

--- ChangeDeskWallpaper/ChangeDeskWallpaper/Program.cs
+++ ChangeDeskWallpaper/ChangeDeskWallpaper/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Win32;
+using System;
 using System.Drawing;
-using System.Windows.Forms;
-using Microsoft.Win32;
 using System.Runtime.InteropServices;
 
 namespace ChangeDeskWallpaper
