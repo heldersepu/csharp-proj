d83d54107100bc01b1e85aa2f74df175dfcf974b
HelderSepu <heldersepu@gmail.com>
2016-11-12 21:05:53 -0500

    Add NLog

--- nhc-noaa/nhc-noaa/Controllers/VideosController.cs
+++ nhc-noaa/nhc-noaa/Controllers/VideosController.cs
@@ -6,12 +6,15 @@ using System.Drawing.Imaging;
 using AviFile;
 using System;
 using System.Dynamic;
+using NLog;
 
 namespace nhc_noaa.Controllers
 {
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class VideosController : BaseController
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         [HttpGet]
         public dynamic EastAtlantic(int count = 20)
         {
@@ -30,27 +33,46 @@ namespace nhc_noaa.Controllers
                 }
                 else
                 {
-                    var bmp = new Bitmap(1120, 480, PixelFormat.Format24bppRgb);
-                    var aviManager = new AviManager(fileName, false);
-                    var aviStream = aviManager.AddVideoStream(true, 25, bmp);
-                    foreach (var file in files)
-                    {
-                        bmp = (Bitmap)Bitmap.FromFile(file.FullName);
-                        aviStream.AddFrame(bmp);
-                        bmp.Dispose();
-                    }
-                    aviManager.Close();
+                    CreateVideo(fileName, files);
                 }
             }
             if (f == null) f = new FileInfo(fileName);
             dynamic obj = new ExpandoObject();
-            obj.File = new ExpandoObject();
             obj.Time = sTime.Diff();
-            obj.File.Name = f.Name;
-            obj.File.Size = f.Length;
-            obj.File.CreationTime = f.CreationTime;
-            obj.File.LastAccessTime = f.LastAccessTime;
+            obj.File = FileDetails(f);
             return Json(obj);
+        }
+
+        private dynamic FileDetails(FileInfo f)
+        {
+            dynamic file = new ExpandoObject();
+            file.Name = f.Name;
+            file.Size = f.Length;
+            file.CreationTime = f.CreationTime;
+            file.LastAccessTime = f.LastAccessTime;
+            return file;
+        }
+
+        private void CreateVideo(string fileName, FileInfo[] files)
+        {
+            try
+            {
+                var bmp = new Bitmap(1120, 480, PixelFormat.Format24bppRgb);
+                var aviManager = new AviManager(fileName, false);
+                var aviStream = aviManager.AddVideoStream(true, 25, bmp);
+                foreach (var file in files)
+                {
+                    bmp = (Bitmap)Bitmap.FromFile(file.FullName);
+                    aviStream.AddFrame(bmp);
+                    bmp.Dispose();
+                }
+                aviManager.Close();
+            }
+            catch (Exception e)
+            {
+                logger.Error(e);
+                throw e;
+            }
         }
     }
 }
