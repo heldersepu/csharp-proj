890156a9f0729b3da3d5d10204ef68dd30a3e86a
HelderSepu <heldersepu@gmail.com>
2016-11-12 20:07:10 -0500

    Return more info in the VideosController

--- nhc-noaa/nhc-noaa/Controllers/VideosController.cs
+++ nhc-noaa/nhc-noaa/Controllers/VideosController.cs
@@ -4,6 +4,8 @@ using System.Web.Http.Cors;
 using System.Drawing;
 using System.Drawing.Imaging;
 using AviFile;
+using System;
+using System.Dynamic;
 
 namespace nhc_noaa.Controllers
 {
@@ -11,28 +13,44 @@ namespace nhc_noaa.Controllers
     public class VideosController : BaseController
     {
         [HttpGet]
-        public string EastAtlantic(int count = 20)
+        public dynamic EastAtlantic(int count = 20)
         {
-            string fileName = string.Empty;
+            FileInfo f = null;
+            DateTime sTime = DateTime.Now;
+            string fileName = baseDir("videos\\");
             var dinfo = new DirectoryInfo(baseDir(east_atl_path));
             var files = dinfo.GetLatestFiles(count);
             if (files.Length > 1)
             {
-                fileName = files[0].Name.Replace(".jpg", "") + "_" + files[files.Length-1].Name.Replace(".jpg", "") + ".avi";
-                var bmp = new Bitmap(1120, 480, PixelFormat.Format24bppRgb);
-                var aviManager = new AviManager(baseDir("videos\\") + fileName, false);
-                var aviStream = aviManager.AddVideoStream(true, 25, bmp);
-
-                foreach (var file in files)
+                fileName += files[0].Name.Replace(".jpg", "") + "_" + files[files.Length-1].Name.Replace(".jpg", "") + ".avi";
+                if (File.Exists(fileName))
                 {
-                    bmp = (Bitmap)Bitmap.FromFile(file.FullName);
-                    aviStream.AddFrame(bmp);
-                    bmp.Dispose();
+                    f = new FileInfo(fileName);
+                    f.LastAccessTime = DateTime.Now;
                 }
-
-                aviManager.Close();
+                else
+                {
+                    var bmp = new Bitmap(1120, 480, PixelFormat.Format24bppRgb);
+                    var aviManager = new AviManager(fileName, false);
+                    var aviStream = aviManager.AddVideoStream(true, 25, bmp);
+                    foreach (var file in files)
+                    {
+                        bmp = (Bitmap)Bitmap.FromFile(file.FullName);
+                        aviStream.AddFrame(bmp);
+                        bmp.Dispose();
+                    }
+                    aviManager.Close();
+                }
             }
-            return fileName;
+            if (f == null) f = new FileInfo(fileName);
+            dynamic obj = new ExpandoObject();
+            obj.File = new ExpandoObject();
+            obj.Time = sTime.Diff();
+            obj.File.Name = f.Name;
+            obj.File.Size = f.Length;
+            obj.File.CreationTime = f.CreationTime;
+            obj.File.LastAccessTime = f.LastAccessTime;
+            return Json(obj);
         }
     }
 }
