44ee95d83f1d6f7c411d1a644f7007e87544d012
HelderSepu <heldersepu@gmail.com>
2017-01-08 21:16:52 -0500

    Add a Decoder solution

---
+++ Decoder/Decoder/Decoder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Decoder
+{
+    public class Decoder
+    {
+        public static int CountDeco(string message, Dictionary<string, string> keys)
+        {
+            int x = 0;
+            foreach (var posibility in Posibilities(message))
+            {
+                bool ok = true;
+                foreach (var key in posibility)
+                {
+                    if (!keys.ContainsKey(key))
+                    {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) x++;
+            }
+            return x;
+        }
+
+        public static List<List<string>> Posibilities(string message)
+        {
+            var x = new List<List<string>>();
+            for (int i = 1; i <= message.Length; i++)
+            {
+                var m = new List<string>();
+                for (int j = 0; j < message.Length; j++)
+                {
+                    try
+                    {
+                        m.Add(message.Substring(j, i));
+                    }
+                    catch { }
+                }
+                x.Add(m);
+            }
+            return x;
+        }
+    }
+}
---
+++ Decoder/DecoderTests/DecoderTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Decoder.Tests
+{
+    [TestClass()]
+    public class DecoderTests
+    {
+        Dictionary<string, string> keys;
+        public DecoderTests()
+        {
+            keys = new Dictionary<string, string>();
+            for (int i = 0; i < 26; i++)
+                keys.Add(i.ToString(), ((char)(i+65)).ToString());
+        }
+
+        [TestMethod()]
+        public void CountDecoTest()
+        {
+            Assert.AreEqual(2, Decoder.CountDeco("12", keys));
+        }
+
+        [TestMethod()]
+        public void PosibilitiesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
