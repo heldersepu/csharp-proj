9bc9f4c7ec7e35d93a60be781c03c16f5b9bd20a
HelderSepu <HelderSepu@gmail.com>
2017-04-03 10:12:14 -0400

    Testing the MS Rest API client

---
+++ offleaseonly/offleaseonly/Program.cs
@@ -0,0 +1,23 @@
+﻿using Offleaseonly;
+using System;
+
+namespace offleaseonly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var offlease = new OffleaseonlyClient();
+            foreach (var c in offlease.Cars.Get(1, 0, "cleanCarFax=1"))
+            {
+                Console.Write(c.Color + " ");
+                Console.Write(c.Make + " ");
+                Console.Write(c.Model + " ");
+                Console.Write(c.Vin + " ");
+            }
+            Console.Read();
+
+        }
+    }
+}
