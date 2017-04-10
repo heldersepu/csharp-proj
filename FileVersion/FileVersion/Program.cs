22cb6ed2a0abc42acd3233f8fb96f911518d2941
HelderSepu <HelderSepu@gmail.com>
2017-04-10 13:57:06 -0400

    Get file version from list of files

---
+++ FileVersion/FileVersion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace FileVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (File.Exists(arg))
+                {
+                    try
+                    {
+                        var versionInfo = FileVersionInfo.GetVersionInfo(arg);
+                        Console.WriteLine(versionInfo.ProductVersion);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("ERROR: " + e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
