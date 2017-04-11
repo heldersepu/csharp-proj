8c6be5cdddbe9a4b4b1bd1d60868188065c8fa3e
HelderSepu <HelderSepu@gmail.com>
2017-04-11 17:00:04 -0400

    Add WorkItemsController

---
+++ TFS_WebApi/TFS_WebApi/Controllers/WorkItemsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TFS_WebApi.Controllers
+{
+    public class WorkItemsController : BaseController
+    {
+        // GET: api/WorkItems?id=2
+        public IHttpActionResult Get(int id)
+        {
+            return Json(witClient.GetWorkItemsAsync(new List<int> { id }).Result);
+        }
+    }
+}
