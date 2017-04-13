3d287f6294abfe45068e3fa1e13e6e174a4194d2
HelderSepu <HelderSepu@gmail.com>
2017-04-13 10:20:58 -0400

    Sample WebApi testing Strathweb.CacheOutput

---
+++ WebApi_StrathwebCacheOutput/WebApi_StrathwebCacheOutput/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using WebApi.OutputCache.V2;
+
+namespace WebApi_StrathwebCacheOutput.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: api/Default
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        //[CacheOutput(ClientTimeSpan = 120, ServerTimeSpan = 120)]
+        [CacheOutput(ClientTimeSpan = 120, ServerTimeSpan = 120, ExcludeQueryStringFromCacheKey = false)]
+        public long Get(int id)
+        {
+            return DateTime.Now.Ticks;
+        }
+    }
+}
