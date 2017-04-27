4e50ed5265d12ce15ce54e2b58ed14f4a13f932e
HelderSepu <HelderSepu@gmail.com>
2017-04-27 14:16:13 -0400

    SetDefaultContentHeaders JSON

--- TFS_WebApi/TFS_WebApi/App_Start/WebApiConfig.cs
+++ TFS_WebApi/TFS_WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace TFS_WebApi
@@ -10,7 +10,7 @@ namespace TFS_WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.Formatters.Add(new BrowserJsonFormatter());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
@@ -19,6 +19,20 @@ namespace TFS_WebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        public class BrowserJsonFormatter : JsonMediaTypeFormatter
+        {
+            public BrowserJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
         }
     }
 }
