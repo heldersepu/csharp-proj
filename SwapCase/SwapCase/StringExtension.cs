e272376d4dc15fdd900784009c03828a60d803c0
HelderSepu <heldersepu@gmail.com>
2017-04-28 22:02:39 -0400

    Add a function to swap the case of a give string

---
+++ SwapCase/SwapCase/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿namespace System
+{
+    public static class StringExtension
+    {
+        public static string SwapCase(this string str)
+        {
+            string x = "";
+            foreach (var c in str)
+            {
+                if ((c >= 'A') && (c <= 'Z'))
+                    x += (char)(c + 32);
+                else if ((c >= 'a') && (c <= 'z'))
+                    x += (char)(c - 32);
+                else
+                    x += c;
+            }
+            return x;
+        }
+    }
+}
---
+++ SwapCase/SwapCaseTests/StringExtensionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Tests
+{
+    [TestClass()]
+    public class StringExtensionTests
+    {
+        [TestMethod()]
+        public void SwapCaseTest()
+        {
+            Assert.AreEqual("Hello", "hELLO".SwapCase());
+            Assert.AreEqual("HelloWorld", "hELLOwORLD".SwapCase());
+            Assert.AreEqual("Hello World", "hELLO wORLD".SwapCase());
+
+            Assert.AreEqual("AAAA", "aaaa".SwapCase());
+            Assert.AreEqual("aaaa", "AAAA".SwapCase());
+
+            Assert.AreEqual("ZZZZ", "zzzz".SwapCase());
+            Assert.AreEqual("zzzz", "ZZZZ".SwapCase());
+
+            Assert.AreEqual("123", "123".SwapCase());
+            Assert.AreEqual("!@#", "!@#".SwapCase());
+        }
+    }
+}
