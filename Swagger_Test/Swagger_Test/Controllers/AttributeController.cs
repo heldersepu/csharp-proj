08191420c7c5fbafc49c1e4162948778e0e8aacd
HelderSepu <HelderSepu@gmail.com>
2017-05-04 09:48:46 -0400

    Testing FromUri & FromBody with Route

---
+++ Swagger_Test/Swagger_Test/Controllers/AttributeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    [RoutePrefix("attrib")]
+    public class AttributeController : ApiController
+    {
+        [Route("{name}")]
+        public void Post([FromUri]string name)
+        {
+        }
+
+        [Route("{id}")]
+        public void Post([FromBody]int id)
+        {
+        }
+    }
+}
