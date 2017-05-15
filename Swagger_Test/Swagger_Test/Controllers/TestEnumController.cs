0135f6d2c026e7e599b3d47e2d39e8201d808b5c
HelderSepu <HelderSepu@gmail.com>
2017-05-15 09:33:31 -0400

    Add TestEnumController

---
+++ Swagger_Test/Swagger_Test/Controllers/TestEnumController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+
+namespace Swagger_Test.Controllers
+{
+    public class TestEnumController : ApiController
+    {
+        public enum CustomEnum
+        {
+            Text = 1,
+            Numeric = 2,
+            Date = 4,
+            Numeric_Function = 8,
+            Dropdown_List = 16,
+            Checkbox = 32
+        }
+
+        public CustomEnum Post(CustomEnum value)
+        {
+            return value;
+        }
+    }
+}
