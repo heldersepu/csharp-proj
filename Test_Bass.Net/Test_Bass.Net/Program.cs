f529c0ae0a25a9ec5df19b049fe2a12c09a56ec2
HelderSepu <HelderSepu@gmail.com>
2017-05-18 13:51:36 -0400

    Add a test to Bass.Net

---
+++ Test_Bass.Net/Test_Bass.Net/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Un4seen.Bass;
+using Un4seen.Bass.AddOn.Tags;
+
+namespace Test_Bass.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BassNet.Registration("heldersepu@gmail.com", "2X3123820152222");
+            foreach (var file in args)
+            {
+                if (File.Exists(file))
+                {
+                    var tagInfo = BassTags.BASS_TAG_GetFromFile(file);
+                    Console.WriteLine("  album: " + tagInfo.album);
+                    Console.WriteLine(" artist: " + tagInfo.artist);
+                    Console.WriteLine("bitrate: " + tagInfo.bitrate);
+                    Console.WriteLine("");
+                }
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadLine();
+        }
+    }
+}
