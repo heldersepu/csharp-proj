eb024a13311f535d0807ba3298366378fe5b6467
HelderSepu <HelderSepu@gmail.com>
2017-05-19 14:42:53 -0400

    Output ASCII data

---
+++ ASCII/ASCII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(i);
+                Console.Write(" ");
+                Console.WriteLine((char)i);
+            }
+            Console.Read();
+        }
+    }
+}
