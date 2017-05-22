a64f43072ce27f0f0c17ce6868ddada59a1dacc9
HelderSepu <HelderSepu@gmail.com>
2017-05-22 14:11:22 -0400

    Change GetById to return ints

--- Swagger_Test/Swagger_Test/Controllers/IHttpActionResultController.cs
+++ Swagger_Test/Swagger_Test/Controllers/IHttpActionResultController.cs
@@ -15,11 +15,11 @@ namespace Swagger_Test.Controllers
         }
 
         // GET: api/IHttpActionResult/{id}
-        [ResponseType(typeof(IEnumerable<string>))]
+        [ResponseType(typeof(IEnumerable<int>))]
         public IHttpActionResult GetById(int id)
         {
             if (id > 0)
-                return Ok(new string[] { "value1", "value2" });
+                return Ok(new int[] { 1, 2 });
             else
                 return BadRequest("id must be greater than 0");
         }
