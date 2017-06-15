1a75ddf622562def9e5f89241d09c203f2c36960
HelderSepu <HelderSepu@gmail.com>
2017-06-14 20:51:37 -0400

    Newtonsoft_ResolveContract
    Project showing how slow the ResolveContract is on a large class

---
+++ Newtonsoft_ResolveContract/Newtonsoft_ResolveContract/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+
+namespace Newtonsoft_ResolveContract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = typeof(HugeClass);
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Reflection GetProperties");
+
+            foreach (var property in type.GetProperties())
+            {
+                var x = property.Name;
+            }
+
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("ResolveContract");
+
+            var resolver = new DefaultContractResolver();
+            JsonContract jsonContract = resolver.ResolveContract(type);            
+
+            Console.WriteLine(DateTime.Now);
+            Console.ReadKey();
+        }
+    }
+}
