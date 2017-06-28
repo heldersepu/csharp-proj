71add290098a1f8b6c4895fef0d1bca68c0a7fef
HelderSepu <HelderSepu@gmail.com>
2017-06-28 09:43:52 -0400

    Add WebApi_MyGet
    Foundation for tests getting packages from myget.org

---
+++ WebApi_MyGet/WebApi_MyGet/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi_MyGet.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: api/Default
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Default/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Default/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Default/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
