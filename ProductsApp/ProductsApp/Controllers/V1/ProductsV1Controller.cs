8da8f5f5a0c0e93424123ef98b699b57cc268cd6
HelderSepu <HelderSepu@gmail.com>
2017-07-03 13:37:15 -0400

    Add ProductsApp project

---
+++ ProductsApp/ProductsApp/Controllers/V1/ProductsV1Controller.cs
@@ -0,0 +1,37 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers.V1
+{
+    [RoutePrefix("api/v1")]
+    public class ProductsV1Controller : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        [Route("Products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [Route("Products/{id}")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductsApp/ProductsApp/Controllers/V2/ProductsV2Controller.cs
@@ -0,0 +1,38 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers.V2
+{
+    [RoutePrefix("api/v2")]
+    public class ProductsV2Controller : ApiController
+    {
+        Product[] products = new Product[] 
+        { 
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M } 
+        };
+
+        [Route("Products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [Route("Products/{id}")]
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
