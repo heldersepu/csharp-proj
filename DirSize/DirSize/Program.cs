9f5fb9a68faa1db4709437ca9ea68966dc63a0ea
HelderSepu <HelderSepu@gmail.com>
2017-07-24 16:29:08 -0400

    Add a project to detect file sizes in a directory

---
+++ DirSize/DirSize/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace DirSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (Directory.Exists(arg))
+                    WriteSize(arg);
+            }
+        }
+
+        static void WriteSize(string path)
+        {
+            var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            int total = 0;
+            using (StreamWriter writetext = new StreamWriter("write.txt"))
+            {
+                foreach (var file in files.OrderBy(x => x.Length))
+                {
+                    writetext.WriteLine($"The file name : {file} is of size {file.Length} bytes");
+                    total += file.Length;
+                }
+                writetext.WriteLine($"The directory size is : {total} bytes");
+            }
+        }
+    }
+}
