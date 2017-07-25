5288412ada3996d73a26b9448c14311d36556925
HelderSepu <HelderSepu@gmail.com>
2017-07-25 11:43:25 -0400

    Add WorkObj

---
+++ TFS_WebApi/TFS_WebApi/Models/WorkObj.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using System.Collections.Generic;
+
+namespace TFS_WebApi.Models
+{
+    public class WorkObj
+    {
+        private WorkItem _item;
+        public WorkItem item
+        {
+            get
+            {
+                _item.Fields = null;
+                _item.Relations = null;
+                _item.Links = null;
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+
+        public List<WorkObj> children { get; set; }
+
+        public WorkObj(WorkItem item)
+        {
+            this.item = item;
+            children = new List<WorkObj>();
+        }
+    }
+}
