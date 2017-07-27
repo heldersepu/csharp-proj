212c1e0098439f8e4a7cf89e75b6cd75c8c59ae6
HelderSepu <HelderSepu@gmail.com>
2017-07-27 10:41:56 -0400

    Simple usage of Task.Start()

---
+++ TaskStart/TaskStart/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            new Task(DoLoop).Start();
+            Console.ReadKey();
+        }
+
+        static void DoLoop()
+        {
+            string chars = "-\\|/-.oO0Oo.";
+            Console.Write("  ");
+            while (1 == 1)
+            {
+                foreach (char chr in chars)
+                {
+                    Console.Write(chr);
+                    Thread.Sleep(100);
+                    Console.Write((char)8);
+                }
+            }
+        }
+    }
+}
