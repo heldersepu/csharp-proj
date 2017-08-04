08e2d53b506ca2fd9fc5b51b06f1d6d6624e4e38
HelderSepu <HelderSepu@gmail.com>
2017-08-04 14:20:11 -0400

    Add routeTemplate to the EnableSwaggerUi

--- WebApi_MyGet/WebApi_MyGet/App_Start/SwaggerConfig.cs
+++ WebApi_MyGet/WebApi_MyGet/App_Start/SwaggerConfig.cs
@@ -193,7 +193,9 @@ namespace WebApi_MyGet
                         //
                         //c.CustomProvider((defaultProvider) => new CachingSwaggerProvider(defaultProvider));
                     })
-                .EnableSwaggerUi(c =>
+                .EnableSwaggerUi(
+                    routeTemplate: "swagger/ui/{*assetPath}",
+                    configure: c =>
                     {
                         // Use the "DocumentTitle" option to change the Document title.
                         // Very helpful when you have multiple Swagger pages open, to tell them apart.
