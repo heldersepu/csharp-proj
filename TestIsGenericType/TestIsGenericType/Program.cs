7a8a76cbd82480a9cec7d7a6cd09cc075f7fec74
HelderSepu <HelderSepu@gmail.com>
2017-08-10 09:43:35 -0400

    Add TestIsGenericType

---
+++ TestIsGenericType/TestIsGenericType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+abstract class Parent<T> {
+    public T Put(T id, char value) {
+        return id;
+    }
+}
+
+class Child : Parent<int> {
+    public string Get(Guid guid) {
+        return "aei";
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        foreach (var methodInfo in typeof(Parent<int>).GetMethods()) {
+            if (!methodInfo.IsVirtual && methodInfo.GetParameters().Length > 0) {
+                Console.WriteLine(methodInfo.Name);
+                foreach (var param in methodInfo.GetParameters()) {
+                    Console.Write("  " + param.Name + " IsGenericType=");
+                    Console.WriteLine(param.ParameterType.IsGenericType);
+                }
+            }
+        }
+        Console.ReadKey();
+    }
+}
