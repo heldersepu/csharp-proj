87b3b17f6640deebaa3b4319d79d6b602daa89db
HelderSepu <heldersepu@gmail.com>
2017-10-22 14:42:27 -0400

    Testing taglib

---
+++ Mp3CustomTag/Mp3CustomTag/Program.cs
@@ -0,0 +1,18 @@
+﻿using TagLib;
+using TagLib.Id3v2;
+
+namespace Mp3CustomTag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = TagLib.File.Create(@"I:\song.mp3");
+            var t = (TagLib.Id3v2.Tag)f.GetTag(TagTypes.Id3v2);
+            var p = PrivateFrame.Get(t, "AlbumType", true);
+            p.PrivateData = System.Text.Encoding.Unicode.GetBytes("TAG CHANGED");
+            f.Tag.Album = "test";
+            f.Save();
+        }
+    }
+}
---
+++ Mp3CustomTag/Mp3CustomTag/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Mp3CustomTag")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mp3CustomTag")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("82b9058d-6d21-448e-9a26-2fda124614c1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
