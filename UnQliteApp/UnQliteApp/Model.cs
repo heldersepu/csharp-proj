ee6ce20fd2e8bc9257493c79a75cf459ee8e12a2
HelderSepu <HelderSepu@gmail.com>
2017-11-07 09:08:13 -0500

    Testing UnQLiteNet

---
+++ UnQliteApp/UnQliteApp/Model.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace UnQliteApp
+{
+    public class Data
+    {
+        static Parent SampleParent(int id)
+        {
+            var p = new Parent { Id = id, Data = "Parent" };
+            for (int i = 0; i < 10; i++)
+                p.Children.Add(new Child { Id = i, Data = $"Child {i}" });
+            return p;
+        }
+    }
+
+    public class Child
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+    }
+
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Data { get; set; }
+        public List<Child> Children { get; set; }
+
+        public Parent()
+        {
+            Children = new List<Child>();
+        }
+    }
+}
---
+++ UnQliteApp/UnQliteApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnQLiteNet;
+
+namespace UnQliteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnQLite unqlite = new UnQLite("test.db", UnQLiteOpenModel.Create | UnQLiteOpenModel.ReadWrite);
+            for (int i = 100; i < 105; i++)
+            {
+                using (var t = unqlite.BeginTransaction())
+                {
+                    unqlite.Save($"key{i}", $"data {i} {i}");
+                }
+            }
+            for (int i = 100; i < 105; i++)
+            {
+                string value = unqlite.Get($"key{i}");
+                Console.WriteLine(value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
