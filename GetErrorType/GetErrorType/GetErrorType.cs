5e75427426e8612035aef6d57ad04e640e882aff
Helder <heldersepu@gmail.com>
2017-12-21 17:15:16 -0500

    Add GetErrorType

---
+++ GetErrorType/GetErrorType/GetErrorType.cs
@@ -0,0 +1,60 @@
+﻿namespace GetErrorType
+{
+    public class GetErrorType
+    {
+        public static string WithIf(ErrorType _errorType)
+        {
+            //Get setting name by passing error type
+            string _eType = string.Empty;
+            if (_errorType == ErrorType.Low)
+                _eType = Constants.LogErrorTypeLow;
+            else if (_errorType == ErrorType.Medium)
+            {
+                _eType = Constants.LogErrorTypeMedium;
+            }
+            else if (_errorType == ErrorType.Critical)
+            {
+                _eType = Constants.LogErrorTypeCritical;
+            }
+            else if (_errorType == ErrorType.High)
+            {
+                _eType = Constants.LogErrorTypeHigh;
+            }
+            return _eType;
+        }
+
+        public static string WithSwitch(ErrorType _errorType)
+        {
+            switch (_errorType)
+            {
+                case ErrorType.High: return Constants.LogErrorTypeHigh;
+                case ErrorType.Critical: return Constants.LogErrorTypeCritical;
+                case ErrorType.Medium: return Constants.LogErrorTypeMedium;
+                default: return Constants.LogErrorTypeLow;
+            }
+        }
+
+        public static string WithToString(ErrorType _errorType)
+        {
+            return "LogErrorType" + _errorType;
+        }
+    }
+
+
+
+    public enum ErrorType
+    {
+        High = 0,
+        Critical = 1,
+        Medium = 2,
+        Low = 3
+    }
+
+    public class Constants
+    {
+        public const string LogErrorTypeLow = "LogErrorTypeLow";
+        public const string LogErrorTypeMedium = "LogErrorTypeMedium";
+        public const string LogErrorTypeHigh = "LogErrorTypeHigh";
+        public const string LogErrorTypeCritical = "LogErrorTypeCritical";
+    }
+}
---
+++ GetErrorType/GetErrorTypeTests/GetErrorTypeTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetErrorType.Tests
+{
+    [TestClass()]
+    public class GetErrorTypeTests
+    {
+        const int max = 1000000;
+
+        [TestMethod()]
+        public void MatchTest()
+        {
+            Assert.AreEqual(GetErrorType.WithIf(ErrorType.High), GetErrorType.WithSwitch(ErrorType.High));
+            Assert.AreEqual(GetErrorType.WithIf(ErrorType.High), GetErrorType.WithToString(ErrorType.High));
+            Assert.AreEqual(GetErrorType.WithIf(ErrorType.Low), GetErrorType.WithSwitch(ErrorType.Low));
+            Assert.AreEqual(GetErrorType.WithIf(ErrorType.Low), GetErrorType.WithToString(ErrorType.Low));
+        }
+
+        [TestMethod()]
+        public void WithIfTest()
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Assert.IsNotNull(GetErrorType.WithIf(ErrorType.High));
+                Assert.IsNotNull(GetErrorType.WithIf(ErrorType.Critical));
+                Assert.IsNotNull(GetErrorType.WithIf(ErrorType.Medium));
+                Assert.IsNotNull(GetErrorType.WithIf(ErrorType.Low));
+            }
+
+        }
+
+        [TestMethod()]
+        public void WithSwitchTest()
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Assert.IsNotNull(GetErrorType.WithSwitch(ErrorType.High));
+                Assert.IsNotNull(GetErrorType.WithSwitch(ErrorType.Critical));
+                Assert.IsNotNull(GetErrorType.WithSwitch(ErrorType.Medium));
+                Assert.IsNotNull(GetErrorType.WithSwitch(ErrorType.Low));
+            }
+        }
+
+        [TestMethod()]
+        public void WithToStringTest()
+        {
+            for (int i = 0; i < max; i++)
+            {
+                Assert.IsNotNull(GetErrorType.WithToString(ErrorType.High));
+                Assert.IsNotNull(GetErrorType.WithToString(ErrorType.Critical));
+                Assert.IsNotNull(GetErrorType.WithToString(ErrorType.Medium));
+                Assert.IsNotNull(GetErrorType.WithToString(ErrorType.Low));
+            }
+        }
+    }
+}
