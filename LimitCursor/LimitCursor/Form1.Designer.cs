1b0ef8a66416c7cfbbd0f62f89d3820d492a82fb
HelderSepu <HelderSepu@gmail.com>
2018-04-13 13:14:45 -0400

    Sample project Limiting the cursor movement

---
+++ LimitCursor/LimitCursor/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LimitCursor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 248);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(296, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(267, 248);
+            this.panel2.TabIndex = 2;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 273);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ LimitCursor/LimitCursor/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LimitCursor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = CenterPoint(panel2);
+        }
+
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = CenterPoint(panel1);
+        }
+
+        private Point CenterPoint(Control control)
+        {
+            return new Point(
+                Left + control.Left + control.Width / 2,
+                Top + control.Top + control.Height / 2);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.X - panel1.Left < 20 && e.Y - panel1.Top < 20)
+            {
+                Cursor.Position = CenterPoint(panel1);
+                Console.WriteLine($"{e.X} {e.Y}");
+            }
+        }
+    }
+}
