12548f44e755398995114358c54e0ee4fed66061
Helder Sepulveda <heldersepu@gmail.com>
2018-05-08 09:30:30 -0400

    Add Swashbuckle.AspNetCore

--- WebApi_NetCore/WebApi_NetCore/Startup.cs
+++ WebApi_NetCore/WebApi_NetCore/Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApi_NetCore
 {
@@ -29,6 +26,10 @@ namespace WebApi_NetCore
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "My Service", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +39,14 @@ namespace WebApi_NetCore
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            app.UseSwagger(c => {
+                c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
+            });
+            app.UseSwaggerUI(c => {
+                c.RoutePrefix = "swagger";
+                // serve the UI at root c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
         }
     }
 }
