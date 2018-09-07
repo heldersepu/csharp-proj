251e0aa5eba1fabbe458f01820766bab84b7292c
Helder Sepulveda <heldersepu@gmail.com>
2018-09-07 18:43:33 -0400

    reproduce issue with inheritance

---
+++ WebApi_NetCore/WebApi_NetCore/Controllers/InherTestController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApi_NetCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/InherTest")]
+    public class InherTestController : BaseController
+    {
+        public InherTestController(string repository) : base(repository)
+        {
+            //In here I will just inherit from the methods from the 
+            //BaseController
+        }
+    }
+
+    public abstract class BaseController : Controller
+    {
+        protected string _repo;
+
+        public BaseController(string repository)
+        {
+            _repo = repository;
+        }
+
+        [HttpGet]
+        public virtual string Get(string repository)
+        {
+            return "test";
+        }
+
+        [HttpGet("{id:int}")]
+        [Route("GetByID")]
+        public async virtual Task<IActionResult> GetByIntID(int id)
+        {
+            try
+            { 
+                return Ok(1);
+            }
+            catch (Exception exp)
+            {
+                return BadRequest("Bad Request");
+            }
+        }
+    }
+}
